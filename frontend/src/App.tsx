973ba05b417b49929b329cffd3f9ed885e5199b5
aaronamano <aaroncedric2005@yahoo.com>
2025-04-03 13:33:23 -0400

    #3 lib page for study sets

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import HorizontalNavbar from "./components/HorizontalNavbar";
 import LibFolders from "./components/LibFolders";
 import VerticalNavbar from "./components/VerticalNavbar";
+import LibStudySets from "./components/LibStudySets";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <div className="bg-[#0A092D] min-h">
         <HorizontalNavbar />
         <VerticalNavbar />
-        <LibFolders />
+        <LibStudySets />
       </div>
     </>
   );
