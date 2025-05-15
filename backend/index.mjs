d990a08e9bdc93ffedda8b5aa94c9695baca7c52
aaronamano <aaroncedric2005@yahoo.com>
2025-05-15 18:12:02 -0400

    changed to import/export format and installed cors

--- backend/index.mjs
+++ backend/index.mjs
@@ -1,10 +1,10 @@
-const express = require('express');
-//import cors from 'cors';
+import express from 'express';
+import cors from 'cors';
 
 const port = 3000;
 const app = express();
 
-//app.use(cors());
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
