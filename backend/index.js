785e2021960d529aabd34244ce02149c71122fe1
aaronamano <aaroncedric2005@yahoo.com>
2025-05-15 13:16:07 -0400

    #11 create express backend

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+//import cors from 'cors';
+
+const port = 3000;
+const app = express();
+
+//app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Press Ctrl+C to stop the server.`);
+})
